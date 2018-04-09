ccf8c01bc15323d70212da75df0a7381274b954d
Moon Antonio <pinchodev91@gmail.com>
2018-04-09 22:09:28 +0200

    Add Setup

--- snake.cpp
+++ snake.cpp
@@ -4,7 +4,29 @@
 
 using namespace std;
 
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirecton dir;
+
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+
 int main()
 {
+    Setup();
+
 	return 0;
 }
